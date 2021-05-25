336d9ecbe48eaf230df30721c555af05ea0e76a6
Григорий Шемчук <schemchuk.grigor@yandex.ru>
2021-05-25 21:28:46 +0300

    Практика 14

--- 11/JsonLogger.php
+++ 11/JsonLogger.php
@@ -1,16 +1,38 @@
 <?php
+
+/**
+ * Пространство имен для объектов логирования
+ */
 namespace Logger;
 
+/**
+ * Стандартный интерфейс для класса лога
+ */
 include "LoggerInterface.php";
 
 use Psr\Log\LoggerInterface as LoggerInterface;
 
 
+/**
+ * Class JsonLogger
+ *
+ * Класс для ведения лога в JSON файле
+ *
+ * @author Gregory
+ * @package Logger
+ * @version 1.0.0
+ */
 class JsonLogger implements LoggerInterface {
 
+    /**
+     * @var string путь к файлу лога
+     */
     private $file = './log.json';
 
 
+    /**
+     * JsonLogger constructor. если JSON файла лога не существует, создает его
+     */
     public function __construct() {
         if (!file_exists($this -> file)) {
             $fp = fopen($this -> file, "w");
@@ -19,42 +41,125 @@ class JsonLogger implements LoggerInterface {
         }
     }
 
+    /**
+     *
+     * лог экстренного сообщения
+     *
+     * @param string $message сообщение
+     * @param array $context данные для форматирования сообщения
+     * @api
+     */
     public function emergency($message, array $context = array()) {
         $this -> doLog('emergency', $message, $context);
     }
 
+    /**
+     *
+     * лог уведомления
+     *
+     * @param string $message сообщение
+     * @param array $context данные для форматирования сообщения
+     * @api
+     */
     public function alert($message, array $context = []) {
         $this -> doLog('alert', $message, $context);
     }
 
+    /**
+     *
+     * лог критического сообщения
+     *
+     * @param string $message сообщение
+     * @param array $context данные для форматирования сообщения
+     * @api
+     */
     public function critical($message, array $context = []) {
         $this -> doLog('critical', $message, $context);
     }
 
+    /**
+     *
+     * лог сообщения об ошибке
+     *
+     * @param string $message сообщение
+     * @param array $context данные для форматирования сообщения
+     * @api
+     */
     public function error($message, array $context = []) {
         $this -> doLog('error', $message, $context);
     }
 
+    /**
+     *
+     * лог предупреждения
+     *
+     * @param string $message сообщение
+     * @param array $context данные для форматирования сообщения
+     * @api
+     */
     public function warning($message, array $context = []) {
         $this -> doLog('warning', $message, $context);
     }
 
+    /**
+     *
+     * лог сообщения-заметки
+     *
+     * @param string $message сообщение
+     * @param array $context данные для форматирования сообщения
+     * @api
+     */
     public function notice($message, array $context = []) {
         $this -> doLog('notice', $message, $context);
     }
 
+    /**
+     *
+     * лог информационного сообщения
+     *
+     * @param string $message сообщение
+     * @param array $context данные для форматирования сообщения
+     * @api
+     */
     public function info($message, array $context = []) {
         $this -> doLog('info', $message, $context);
     }
 
+    /**
+     *
+     * лог сообщения отладки
+     *
+     * @param string $message сообщение
+     * @param array $context данные для форматирования сообщения
+     * @api
+     */
     public function debug($message, array $context = []) {
         $this -> doLog('debug', $message, $context);
     }
 
+    /**
+     *
+     * лог обычного сообщения для логирования
+     *
+     * @param $level string задание уровня лога
+     * @param string $message сообщение
+     * @param array $context данные для форматирования сообщения
+     * @api
+     */
     public function log($level, $message, array $context = []) {
         $this -> doLog('log', $message, $context);
     }
 
+    /**
+     *
+     * метод записи сообщения в файл лога
+     *
+     * @param $type string тип логируемого сообщения
+     * @param $message string тело сообщения для лога
+     * @param array $context данные для форматирования сообщения
+     * @todo вынести формирование тела объекта в новый метод
+     * @since 1.0.0
+     */
     private function doLog($type, $message, array $context = array()) {
         if (file_get_contents($this -> file) && file_get_contents($this -> file) == "[]") {
             file_put_contents($this -> file, "[");
--- 11/LoggerInterface.php
+++ 11/LoggerInterface.php
@@ -1,16 +1,108 @@
 <?php
 
+/**
+ * пространство имен для описания функционала логировния
+ */
 namespace Psr\Log;
 
 
+/**
+ * Interface LoggerInterface
+ *
+ * стандартный интейрфес лога
+ *
+ * @package Psr\Log
+ * @version 1.0.0
+ */
 interface LoggerInterface {
+    /**
+     *
+     * метод записи сообщения в лог
+     *
+     * @param $message string сообщение лога
+     * @param array $context данные для форматирования сообщения
+     * @return void
+     */
     public function emergency($message, array $context = []);
+
+    /**
+     *
+     * метод записи сообщения в лог
+     *
+     * @param $message string сообщение лога
+     * @param array $context данные для форматирования сообщения
+     * @return void
+     */
     public function alert($message, array $context = []);
+
+    /**
+     *
+     * метод записи сообщения в лог
+     *
+     * @param $message string сообщение лога
+     * @param array $context данные для форматирования сообщения
+     * @return void
+     */
     public function critical($message, array $context = []);
+
+    /**
+     *
+     * метод записи сообщения в лог
+     *
+     * @param $message string сообщение лога
+     * @param array $context данные для форматирования сообщения
+     * @return void
+     */
     public function error($message, array $context = []);
+
+    /**
+     *
+     * метод записи сообщения в лог
+     *
+     * @param $message string сообщение лога
+     * @param array $context данные для форматирования сообщения
+     * @return void
+     */
     public function warning($message, array $context = []);
+
+    /**
+     *
+     * метод записи сообщения в лог
+     *
+     * @param $message string сообщение лога
+     * @param array $context данные для форматирования сообщения
+     * @return void
+     */
     public function notice($message, array $context = []);
+
+    /**
+     *
+     * метод записи сообщения в лог
+     *
+     * @param $message string сообщение лога
+     * @param array $context данные для форматирования сообщения
+     * @return void
+     */
     public function info($message, array $context = []);
+
+    /**
+     *
+     * метод записи сообщения в лог
+     *
+     * @param $message string сообщение лога
+     * @param array $context данные для форматирования сообщения
+     * @return void
+     */
     public function debug($message, array $context = []);
+
+    /**
+     *
+     * метод записи сообщения в лог
+     *
+     * @param $level string задание уровня лога
+     * @param $message string сообщение лога
+     * @param array $context данные для форматирования сообщения
+     * @return void
+     */
     public function log($level, $message, array $context = []);
 }
